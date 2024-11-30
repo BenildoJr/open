fa3546464a1e16562de12f3ec0f2e8754dbdc2de
Benildo Junior <benildo@outlook.com>
2024-11-30 17:10:19 -0300

    Primeiro botão

---
+++ script.js
@@ -0,0 +1,5 @@
+// Seleciona o botão e define a ação de clique
+document.getElementById("testeButton").addEventListener("click", function() {
+    // Exibe um alerta com uma mensagem
+    alert("Você clicou no botão!");
+});
